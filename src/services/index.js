378f0104c957cd6a172bdae5b7613c818b727ddb
Christian Bueschi <christian.bueschi@namics.com>
2016-04-21 15:15:52 +0200

    clean

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,6 @@
 'use strict';
 const notification = require('./notification');
 const s3 = require('./s3');
-const authentication = require('./authentication');
-const user = require('./user');
 const mongoose = require('mongoose');
 module.exports = function() {
   const app = this;
@@ -10,8 +8,6 @@ module.exports = function() {
   mongoose.connect(app.get('mongodb'));
   mongoose.Promise = global.Promise;
 
-  app.configure(authentication);
-  app.configure(user);
   app.configure(notification);
   app.configure(s3);
 };
