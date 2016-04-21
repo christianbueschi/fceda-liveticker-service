ed62b6d2e1d4fcce2c48b63cd51e128366736eae
Christian Bueschi <christian.bueschi@namics.com>
2016-04-21 15:47:49 +0200

    add logs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
+console.log('port: ', port);
+console.log('host: ', app.get('host'));
+
+
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
 );
